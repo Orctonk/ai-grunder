2f721a141bae944931129522a583f90296f78ee3
Filip Henningsson <c18fhn@cs.umu.se>
2020-09-24 13:50:11 +0200

    Finished KNN

--- knn/main.py
+++ knn/main.py
@@ -4,22 +4,23 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 colors = ["b","g","r","c","m","y","k","w"]
-exclude = [0,1,2]
 
-def main():
-    mpl.use("TkAgg")
+def get_iris_scores(k_max,split):
+    exclude = [0,1,2,4,5]
+
     features = [0,1,2,3]
-    k_max = 25
+    feature_combinations = combinations(features,2)
+
     train_scores = []
     test_scores = []
-    
-    feature_combinations = combinations(features,2)
+
     combstr = []
     fnum = 0
+
     for (i,c) in enumerate(feature_combinations):
         if i in exclude:
             continue
-        x_train,x_test,y_train,y_test = load_dataset("iris",c,0.66)
+        x_train,x_test,y_train,y_test = load_dataset("iris",c,split)
         combstr.append(str(c))
         train_scores.append([])
         test_scores.append([])
@@ -30,6 +31,42 @@ def main():
             train_scores[fnum].append(train)
             test_scores[fnum].append(test)
         fnum += 1
+
+    return (train_scores,test_scores,combstr)
+
+def get_cancer_scores(k_max,split):
+    features = [x for x in range(30)]
+    exclude = [1,4,5,8,9,10,11,12,14,15,16,17,18,19,21,24,25,26,28,29]
+
+    train_scores = []
+    test_scores = []
+
+    combstr = []
+
+    features = []
+    for i in range(30):
+        if not i in exclude:
+            features.append(i)
+
+    x_train,x_test,y_train,y_test = load_dataset("breastcancer",features,split)
+    combstr.append(str(features))
+    train_scores.append([])
+    test_scores.append([])
+    
+    for k in range(1,k_max+1):
+        knn = train_knn(x_train,y_train,k)
+        (train,test) = evaluate_knn(knn,x_train,y_train,x_test,y_test)
+        train_scores[0].append(train)
+        test_scores[0].append(test)
+
+    return train_scores,test_scores,combstr
+
+def main():
+    mpl.use("TkAgg")
+    k_max = 30
+
+    #(train_scores,test_scores,combstr) = get_iris_scores(k_max,0.66)
+    (train_scores,test_scores,combstr) = get_cancer_scores(k_max,0.66)
 
     # Construct plot
     plt.title('KNN results')
