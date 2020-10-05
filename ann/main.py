d24f21f39e2bd91110f8898081d7af5fdbd80bc6
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-10-05 19:22:46 +0200

    Added L2 regularization to main

--- ann/main.py
+++ ann/main.py
@@ -8,7 +8,7 @@ def main():
     (train_images,train_labels,test_images,test_labels) = load_data()
     train_labels_ohv = labels_to_1_hot(train_labels)
 
-    network = ANN(784, 'mean_square_error')
+    network = ANN(784, 'mean_square_error',regularization='L2',lambd=0.005)
     network.add_layer(3,'leaky_relu')
     network.add_layer(3,'leaky_relu')
     network.add_layer(10, 'tanh')
@@ -17,19 +17,21 @@ def main():
 
     test_accuracy = calculate_accuracy(test_labels,test_out.argmax(1))
     print("Test accuracy: {}%".format(test_accuracy * 100))
-    print_class_count(test_out)
 
-    loss = network.train(train_images,train_labels_ohv,1000,10,0.15)
+    loss = network.train(train_images,train_labels_ohv,1000,100,0.1)
     xaxis = [x for x in range(1000)]
 
     plt.plot(xaxis,loss)
     plt.show()
 
+    train_out = network.eval(train_images)
     test_out = network.eval(test_images)
+
+    train_accuracy = calculate_accuracy(train_labels,train_out.argmax(1))
+    print("Train accuracy: {}%".format(train_accuracy * 100))
 
     test_accuracy = calculate_accuracy(test_labels,test_out.argmax(1))
     print("Test accuracy: {}%".format(test_accuracy * 100))
-    print_class_count(test_out)
     
 
 if __name__ == "__main__":
