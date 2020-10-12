8dcc33d5c8f60382ecce8d3a7c88d5bbcf40428d
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-10-12 09:31:46 +0200

    Commented util.py

--- ann/util.py
+++ ann/util.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 def read_labels(path):
+    """Reads the label file provided
+    
+    Parameters
+    ----------
+    path: The path to the label file
+
+    Return
+    ------
+    The labels read from the file
+    """
     file = open(path,"r")
 
     reading_labels = False
@@ -20,6 +30,16 @@ def read_labels(path):
     return np.array(labels)
 
 def read_images(path):
+    """Reads the image file provided
+    
+    Parameters
+    ----------
+    path: The path to the image file
+
+    Return
+    ------
+    The image read from the file
+    """
     file = open(path,"r")
 
     reading_images = False
@@ -39,6 +59,19 @@ def read_images(path):
     return np.array(images)
 
 def load_data(traini_path,trainl_path,testi_path,testl_path=None):
+    """Loads data from files
+
+    Parameters
+    ----------
+    traini_path: The path to the training images.
+    trainl_path: The path to the training labels.
+    testi_path: The path to the test images.
+    testl_path: The path to the test labels.
+
+    Return
+    ------
+    A tuple with the loaded data in the same order as the arguments
+    """
     traini = read_images(traini_path)
     testi = read_images(testi_path)
 
@@ -55,6 +88,16 @@ def load_data(traini_path,trainl_path,testi_path,testl_path=None):
     return (traini,trainl,testi,testl)
 
 def labels_to_1_hot(labels):
+    """Processes labels into a more ANN-friendly format
+
+    Parameters
+    ----------
+    labels: The labels to process
+
+    Return
+    ------
+    An array where each label is substituted for a vector with a '1' on the position representing the label
+    """
     # Process labels to 1-hot form
     # see https://stackoverflow.com/questions/29831489/convert-array-of-indices-to-1-hot-encoded-numpy-array
     labels_ohv = np.zeros((labels.size,labels.max()+1))
@@ -62,11 +105,13 @@ def labels_to_1_hot(labels):
     return labels_ohv
 
 def calculate_accuracy(target,classifications):
+    """Calculates the accuracy of the classifications.
+
+    Parameters
+    ----------
+    target: The target classifications.
+    classification: The ANN classifications
+    """
     accuracy_arr = np.zeros(target.shape)
     accuracy_arr[target==classifications] = 1
     return accuracy_arr.sum() / accuracy_arr.size
-
-def print_class_count(classifications):
-    class_count = np.zeros_like(classifications)
-    class_count[np.arange(class_count.shape[0]),classifications.argmax(1)] = 1.0
-    print(class_count.sum(0))
